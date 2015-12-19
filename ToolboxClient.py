3e7d7a8588e7faa7a0f1a0d1e958e022843259ba
ercjns <eric@ercjns.com>
2015-12-18 23:37:48 -0800

    toolbox folder monitor
    also some refactoring of the toolbox functions

--- ToolboxClient.py
+++ ToolboxClient.py
@@ -12,19 +12,26 @@ import json
 if __name__ == '__main__':
     host = sys.argv[1]
     method = sys.argv[2]
-    arg = sys.argv[3]
     
     if method == 'iof3':
-    # post sport software OE2012 I0F3 results file
-        url = host + '/api/results/'
-        f = {'file': open(arg, 'r')}
-        #What should be in this header???
-        #header = {'content-type': 'text/plain'}
-        r = requests.post(url, files=f)
-        print r.text
-
-    if method == 'telemetry':
-    # fake a remote punch call
+        ''' 
+        post an iof3 resultList file to refresh the data on the server
+        python ToolboxClient.py http://localhost iof3 testdata.xml 
+        '''
+        post_iof3_resultList(host, sys.argv[3])
+    
+    elif method == 'monitor':
+        ''' 
+        monitor a folder for new results and post them
+        usage: python ToolboxClient.py http://localhost monitor ./results_go_here 
+        '''
+        poll_for_results(sys.argv[3])
+        
+    elif method == 'telemetry':
+        ''' 
+        mimic a call coming in from a remote punch box
+        python ToolboxClient.py http://localhost telemetry 17 99999 "09:42:17" 
+        '''
         d = {}
         d['station'] = sys.argv[3]
         d['sicard'] = sys.argv[4]
@@ -33,3 +40,30 @@ if __name__ == '__main__':
         header = {'content-type': 'text/json'}
         r = requests.post(url, headers=header, data=json.dumps(d))
         print r.text
+
+def post_iof3_resultList(host, file):
+    # post sport software IOFv3 results file
+    url = host + '/api/results/'
+    f = {'file': open(file, 'r')}
+    #What should be in this header???
+    #header = {'content-type': 'text/plain'}
+    r = requests.post(url, files=f)
+    return r.text
+            
+def poll_for_results(dir):
+    lastUpdate = None
+    while True:
+        dircontents = os.listdir(dir)
+        if len(dircontents) == 1:
+            # TODO actually get the right file rather than picking the only one!
+            fn = os.path.join(dir, dircontents[0])
+            t = os.path.getmtime(fn)
+            if t > lastUpdate:
+                print "found new file, posting"
+                postToMeetWeb(fn)
+                lastUpdate = t
+            else:
+                print "no new file"
+        print "sleeping, brb"
+        for i in range(60):
+            time.sleep(1)
