a762392392e3e5ae9fcb029b87176f9271fed54d
ercjns <eric@ercjns.com>
2016-01-08 21:23:50 -0800

    Meet Stats view

--- coc_wifiscoring/frontendcontroller.py
+++ coc_wifiscoring/frontendcontroller.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, abort, render_template
 
-from .models import db, Result
+from .models import db, Result, RemotePunch
 
 frontend = Blueprint("frontend", __name__)
 
@@ -20,3 +20,24 @@ def cclass_results(cclass):
 def all_results():
     q = Result.query.all()
     return render_template('allresults.html', items=q)
+
+@frontend.route('/meetstats/')
+def meet_stats():
+    download_items = Result.query.all()
+    download_sicards = [a.sicard for a in download_items]
+    
+    check_items = RemotePunch.query.filter_by(station=17).group_by(RemotePunch.sicard).order_by(RemotePunch.time).all()
+    
+    fin = len(download_sicards)
+    checked = 0
+    
+    out_items = []
+    for card in check_items:
+        if card.sicard not in download_sicards:
+            checked += 1
+            out_items.append(card)
+            continue
+
+    checked += fin
+    
+    return render_template('meetstats.html', checked=checked, downloaded=len(download_sicards), out=checked-fin, items=out_items)
