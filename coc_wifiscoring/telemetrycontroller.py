ee30a12e2eb00258f0824fab956ec9f59fdebed1
ercjns <eric@ercjns.com>
2016-02-05 16:37:14 -0800

    Populate name in real-time

--- coc_wifiscoring/telemetrycontroller.py
+++ coc_wifiscoring/telemetrycontroller.py
@@ -1,17 +1,21 @@
 from flask import Blueprint, request, abort, render_template
 from datetime import time
 
-from .models import db, RemotePunch, Result
+from .models import db, RemotePunch, Result, Entry
 
 from coc_wifiscoring import socketio
 
 telemetry = Blueprint("telemetry", __name__)
 
-@telemetry.route('/<int:control>')
+@telemetry.route('/<int:control>', methods=['GET'])
 def get_punches(control):
     try:
-        q = RemotePunch.query.filter_by(station=control).all()
-        return render_template('telemetry.html', box=control, items=q)
+        punches = RemotePunch.query.filter_by(station=control).all()
+        silookup = {}
+        entries = Entry.query.all()
+        for e in entries:
+            silookup[e.sicard] = e.name
+        return render_template('telemetry.html', box=control, punches=punches, names=silookup)
     except:
         abort(404)
 
@@ -24,7 +28,9 @@ def record_punch(control):
         punch = RemotePunch(body['station'], body['sicard'], time(int(h),int(m),int(s)))
         db.session.add(punch)
         db.session.commit()
-        socketio.emit('new punch', {'station': body['station'], 'sicard':body['sicard'], 'time':body['time']})
+        entry = Entry.query.filter_by(sicard=body['sicard']).first()
+        name = entry.name if entry else ''
+        socketio.emit('new punch', {'station': body['station'], 'sicard':body['sicard'], 'time':body['time'], 'name':name})
         return str(punch), 200
     except:
         abort(500)
